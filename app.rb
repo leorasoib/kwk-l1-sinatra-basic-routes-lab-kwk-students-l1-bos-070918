e1c7536da69c81c04f7b4ea8fe8fa003280268c9
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-17 14:01:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,9 +9,9 @@ get '/name' do
 "My name is "
 end 
 get '/hometown' do 
-"My hometown is_"
+"My hometown is "
 end 
 get '/favorite-song' do 
-  "My favorite song is_"
+  "My favorite song is "
 end 
 end
